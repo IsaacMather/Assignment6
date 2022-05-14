404089afe611ac476b0073927a34160d03478f58
Isaac <idmather@ucdavis.edu>
2022-05-14 13:47:45 -0700

    added notes for the block checker

--- BlockClass.py
+++ BlockClass.py
@@ -6,9 +6,6 @@ class Block:
         self._hash = hash((self._data, self._prev_hash))
         self.print_hash()
 
-
-
-
     #the init method should save the data parameter into self_data and
     # create a new self_hash using a hashed combination of data and
     # prev_hash. self._next should be none
@@ -38,6 +35,8 @@ class Block:
         # you need to make it so it reshashes the self.data and
         # self.previous, and then checks it against the current self.hash to
         # see if anything has changed
+        print()
+        print("Replaying Blockchain")
         current = self
         # print(self._data)
         while current._next is not None:
@@ -47,6 +46,7 @@ class Block:
 
         if current._next is None:
             print(current._data)
+            print(f'Final hash is {current._hash}')
 
     #the add_block method will traverse the list to search for the last
     # block. It will create a new Block object using the data parameter  and
@@ -54,7 +54,40 @@ class Block:
     # the last block.
 
     def replay(self, prev_hash=0):
+        # print()
+        print("Replaying Blockchain")
+        # you need to make it so it reshashes the self.data and
+        # self.previous, and then checks it against the current self.hash to
+        # see if anything has changed
+        current = self
+        # print(self._data)
+        while current._next is not None:
+            hash_check = hash((current._data, current._prev_hash))
+            # print(hash_check)
+            # print(current._hash)
+            if hash_check == current._hash:
+                print(current._data)
+                current = current._next
+            else:
+                print("Blockchain is Corrupted!!")
+                # print(current._data)
+                return
 
+        if current._next is None:
+            hash_check = hash((current._data, current._prev_hash))
+            if hash_check == current._hash:
+                print(current._data)
+                print("Blockchain verified. End of Transactions")
+                print(f'Final hash is {current._hash}')
+                return
+
+            else:
+                print("Blockchain is Corrupted!!")
+                # print(current._data)
+                return
+
+
+    # the add_block method will traverse the l
 
     #the replay method will traverse the list. At each block it first
     # verifies self._hash. If the calculate value does not match self_hash,
--- main.py
+++ main.py
@@ -6,17 +6,17 @@ def block_tester():
     new_chain.add_block("Eric Pays the cat $50 for ending the noise")
     new_chain.add_block("The cat pays Pet Food Express $20 for catnip")
     print()
-    new_chain.block_checker()
-    # new_chain.replay()
-    # print()
-    #
-    # # Cat tried to hack the chain
-    # fraud = "Eric Pays the cat $500 for ending the noise"
-    # new_chain._next._next._data = fraud
-    # new_chain.replay()
+    new_chain.replay()
+    print()
+
+    # Cat tried to hack the chain
+    fraud = "Eric Pays the cat $500 for ending the noise"
+    new_chain._next._next._data = fraud
+    new_chain.replay()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     block_tester()
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
